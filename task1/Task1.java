6b70fdb4878cf2339480dee913d75790040001a8
Kras-Dev <114261841+Kras-Dev@users.noreply.github.com>
2023-12-18 02:07:59 +0300

    Update Task1.java

--- task1/Task1.java
+++ task1/Task1.java
@@ -6,7 +6,7 @@ public class Task1 {
         }
         int n = Integer.parseInt(args[0]); // Размер кругового массива
         int m = Integer.parseInt(args[1]); // Длина интервала обхода
-
+	// Заполняем массив числами от 1 до n
         int[] circularArray = new int[n];
         for (int i = 0; i < n; i++) {
             circularArray[i] = i + 1;
@@ -14,6 +14,7 @@ public class Task1 {
         int currentIndex = 0; 
         do {
             System.out.print(circularArray[currentIndex] + " ");
+	//обновляем currentIndex для определения следующего элемента
             current = (currentIndex + m - 1) % n;
         } while (currentIndex != 0);
     }
